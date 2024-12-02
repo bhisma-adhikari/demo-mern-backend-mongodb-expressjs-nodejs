d21d0166856ac3b153c9f21d15903eb57c389821
Bhisma <bhismaworks@gmail.com>
2024-12-02 11:38:28 -0600

    added nodemon

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/v1/users', (req, res) => {
+  res.json([]);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
